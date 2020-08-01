3b7d59c467816e17d01bc00c64253f89a2ebff88
Yongguang <mayongguang86@gmail.com>
2020-08-01 12:52:13 -0400

    refactor ut

---
+++ src/test/java/com/example/CityAutocomplete/ApiBaseTest.java
@@ -0,0 +1,42 @@
+package com.example.CityAutocomplete;
+
+import com.example.CityAutocomplete.service.load.CityLoader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiBaseTest {
+
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected MockMvc mockMvc;
+    @MockBean
+    protected CityLoader cityLoader;
+
+    @BeforeEach
+    protected void beforeEachBase() {
+        objectMapper = new ObjectMapper();
+    }
+
+    protected <TResponse> TResponse callApi(String url, Class<TResponse> responseClass) throws Exception {
+        String contentAsString = mockMvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        return objectMapper.readValue(contentAsString, responseClass);
+    }
+}
