31692978775d38aea949d0fa00fde37523f00701
Yongguang <mayongguang86@gmail.com>
2020-07-31 20:52:19 -0400

    initialize spring boot

---
+++ src/main/java/com/example/CityAutocomplete/CityAutocompleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CityAutocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityAutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityAutocompleteApplication.class, args);
+	}
+
+}
